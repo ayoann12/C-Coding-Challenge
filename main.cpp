b17d5f9c24f9e9bdeac95b70132c5fb26149159f
unknown <ayoann12@yahoo.fr>
2019-03-12 12:37:25 +0100

    **FINAL COMMIT**(normally)

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include<string>
-#include<vector>
 #include<stack>
-#include<sstream>
 #include <iostream>
-#include <cstdlib>
-#include <typeinfo>
 using namespace std;
 
+/*
+    "error" is a code that is returned when an unexpected character is found
+    (everything but digits or operands or braces)
+*/
 float error = 0.0110011;
 
+
+/*
+    "priority" is a function that ranks the operand's priority. So "*" and "/" are prior over "+" and "-",
+    and those last are prior and opening brace
+*/
 int priority(char op){
     if(op == '+' || op == '-')
     {
@@ -23,6 +28,10 @@ int priority(char op){
     return 0;
 }
 
+/*
+    "operation" is a function that applies the operation, according to the operand.
+    It detects also unexpected characters and returns error code
+*/
 float operation(char op, float number1, float number2)
 {
     switch (op)
@@ -54,30 +63,28 @@ int main()
 
     std::cin >> expression;
 
+    // We add braces all around the expression for better processing
     expression = std::string("(") + expression + std::string(")");
 
+    // "numbers" is a stack to store the numbers of the expression. "ops" stores braces and operands.
     std::stack<float> numbers;
-
     std::stack<char> ops;
 
+    // "number" stores current number before stacking it
+    float number = 0;
     int i = 0;
 
-    int temp = 0;
-
-    float number = 0;
 
     while(i < expression.size())
     {
         if(expression[i] == '(')
         {
-            std::cout<<"Case 1 : opening brace"<<std::endl;
             ops.push(expression[i]);
             i++;
         }
 
         else if(isdigit(expression[i]))
         {
-            std::cout<<"Case 2 : digit"<<std::endl;
             while(isdigit(expression[i]) && i < expression.size())
             {
                 number = number * 10 + (expression[i] - '0');
@@ -89,7 +96,10 @@ int main()
 
         else if(expression[i] == ')')
         {
-            std::cout<<"Case 3 : closing brace"<<std::endl;
+            /*
+                When the program finds a closing brace, it computes everything before until the very previous opening brace.
+                It pops also everything computed and stacks the result
+            */
             while(!ops.empty() && ops.top() != '(')
             {
                 float value2 = numbers.top();
@@ -103,21 +113,28 @@ int main()
 
                 numbers.push(operation(op, value1, value2));
             }
-            // pop opening brace.
+
+            // It pops also the opening brace
             ops.pop();
             i++;
         }
 
         else
         {
-            if(operation(expression[i], 0, 0) == error)
+            /*
+                if error code is returned or two operands are following, the program stops
+            */
+            if(operation(expression[i], 0, 0) == error || !isdigit(expression[i-1]))
             {
-                std::cout<<"Expression invalide !"<<std::endl;
+                std::cout<<"Invalid expression!"<<std::endl;
                 return 0;
             }
+
+            /*
+                else it computes the two numbers on top if the priority is correct
+            */
             else
             {
-                std::cout<<"Case 4 : operation"<<std::endl;
                 while(!ops.empty() && priority(ops.top()) >= priority(expression[i]))
                 {
                     float value2 = numbers.top();
@@ -136,10 +153,20 @@ int main()
             i++;
 
         }
-        std::cout<<i<<std::endl;
     }
 
-    std::cout<<numbers.top()<<std::endl;
+    // Normally, the final result only is in the stack
+    float result = numbers.top();
+    numbers.pop();
+
+    // Else, the program stops with an error
+    if (!numbers.empty())
+    {
+        std::cout<<"Invalid expression!"<<std::endl;
+        return 0;
+    }
+
+    std::cout<<result<<std::endl;
 
     return 0;
 }
