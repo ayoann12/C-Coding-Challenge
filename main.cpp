5c6d9edeb748fb4fba3f885273b9755f5ffd6af2
unknown <ayoann12@yahoo.fr>
2019-03-12 12:09:36 +0100

    Task : All operations, with handling parentheses

--- main.cpp
+++ main.cpp
@@ -4,31 +4,47 @@
 #include<sstream>
 #include <iostream>
 #include <cstdlib>
+#include <typeinfo>
 using namespace std;
 
-std::vector<std::string> split(std::string strToSplit, char delimeter)
-{
-    std::stringstream ss(strToSplit);
-    std::string item;
-	std::vector<std::string> splittedStrings;
-    while (std::getline(ss, item, delimeter))
-	{
-		splittedStrings.push_back(item);
-    }
-	return splittedStrings;
-}
+float error = 0.0110011;
 
-bool verify(std::string strToVerify)
-{
-    for (int i = 0; i < strToVerify.size(); i++){
-        if(strToVerify[i] < '0' && strToVerify[i] > '9' && strToVerify[i] != '+' && strToVerify[i] != '-' && strToVerify[i] != '*' && strToVerify[i] != '/')
-        {
-            return false;
-        }
+int priority(char op){
+    if(op == '+' || op == '-')
+    {
+        return 1;
     }
 
-    return true;
+    if(op == '*'||op == '/')
+    {
+        return 2;
+    }
+
+    return 0;
 }
+
+float operation(char op, float number1, float number2)
+{
+    switch (op)
+    {
+        case '+':
+            return number1 + number2;
+            break;
+        case '-':
+            return number1 - number2;
+            break;
+        case '*':
+            return number1 * number2;
+            break;
+        case '/':
+            return number1 / number2;
+            break;
+        default:
+            return error;
+            break;
+    }
+}
+
 
 int main()
 {
@@ -38,149 +54,92 @@ int main()
 
     std::cin >> expression;
 
-    std::cout<<verify(expression)<<std::endl;
+    expression = std::string("(") + expression + std::string(")");
 
-    if (verify(expression))
+    std::stack<float> numbers;
+
+    std::stack<char> ops;
+
+    int i = 0;
+
+    int temp = 0;
+
+    float number = 0;
+
+    while(i < expression.size())
     {
-        std::vector<std::string> split_plus = split(expression, '+');
-        std::string expression_without_plus("");
-        for (int i = 0; i < split_plus.size(); i++)
+        if(expression[i] == '(')
         {
-            expression_without_plus += std::string("-") + split_plus[i];
+            std::cout<<"Case 1 : opening brace"<<std::endl;
+            ops.push(expression[i]);
+            i++;
         }
 
-        std::string expression_minus = expression_without_plus.substr(1);
-        std::vector<std::string> split_minus = split(expression_minus, '-');
-        std::string expression_without_minus("");
-        for (int i = 0; i < split_minus.size(); i++)
+        else if(isdigit(expression[i]))
         {
-            expression_without_minus += std::string("*") + split_minus[i];
-        }
-
-        std::string expression_times = expression_without_minus.substr(1);
-        std::vector<std::string> split_times = split(expression_times, '*');
-        std::string expression_without_times("");
-        for (int i = 0; i < split_times.size(); i++)
-        {
-            expression_without_times += std::string("/") + split_times[i];
-        }
-
-        std::string expression_on = expression_without_times.substr(1);
-        std::vector<std::string> split_on = split(expression_on, '/');
-
-        std::vector<float> numbers;
-        std::vector<char> ops;
-        float temp = 0;
-
-        for (int i = 0; i < split_on.size(); i++)
-        {
-            std::stringstream express(split_on[i]);
-            express >> temp;
-            numbers.push_back(temp);
-            temp = 0;
-        }
-        for (int i = 0; i < expression.size(); i++)
-        {
-            if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/')
+            std::cout<<"Case 2 : digit"<<std::endl;
+            while(isdigit(expression[i]) && i < expression.size())
             {
-                ops.push_back(expression[i]);
+                number = number * 10 + (expression[i] - '0');
+                i++;
             }
+            numbers.push(number);
+            number = 0;
         }
 
-        std::stack<float> mystack;
-        mystack.push(numbers[0]);
-
-        for (int i = 0; i < ops.size(); i++)
+        else if(expression[i] == ')')
         {
-            if(ops[i] == '*')
+            std::cout<<"Case 3 : closing brace"<<std::endl;
+            while(!ops.empty() && ops.top() != '(')
             {
-                temp = mystack.top() * numbers[i+1];
-                mystack.pop();
-                mystack.push(temp);
+                float value2 = numbers.top();
+                numbers.pop();
+
+                float value1 = numbers.top();
+                numbers.pop();
+
+                char op = ops.top();
+                ops.pop();
+
+                numbers.push(operation(op, value1, value2));
             }
-            else if(ops[i] == '/')
+            // pop opening brace.
+            ops.pop();
+            i++;
+        }
+
+        else
+        {
+            if(operation(expression[i], 0, 0) == error)
             {
-                temp = mystack.top() / numbers[i+1];
-                mystack.pop();
-                mystack.push(temp);
+                std::cout<<"Expression invalide !"<<std::endl;
+                return 0;
             }
             else
             {
-                mystack.push(numbers[i+1]);
+                std::cout<<"Case 4 : operation"<<std::endl;
+                while(!ops.empty() && priority(ops.top()) >= priority(expression[i]))
+                {
+                    float value2 = numbers.top();
+                    numbers.pop();
+
+                    float value1 = numbers.top();
+                    numbers.pop();
+
+                    char op = ops.top();
+                    ops.pop();
+
+                    numbers.push(operation(op, value1, value2));
+                }
+                ops.push(expression[i]);
             }
-        }
+            i++;
 
-        float final_result = mystack.top();
-        mystack.pop();
-
-        for (int i = ops.size()-1; i >= 0 && !mystack.empty(); i--)
-        {
-            if(ops[i] == '+')
-            {
-                final_result = mystack.top() + final_result;
-                mystack.pop();
-            }
-            else if(ops[i] == '-')
-            {
-                final_result = mystack.top() - final_result;
-                mystack.pop();
-            }
         }
-        std::cout<<final_result<<std::endl;
-    }
-    else
-    {
-        std::cout<<"Error !!! Your expression is invalid"<<std::endl;
+        std::cout<<i<<std::endl;
     }
 
-    /*int j = 1;
-    int previous_time = 0;
-
-
-    int final_result = result[0];
-
-    for (int i = 0; i < expression.size(); i++)
-    {
-        if (expression[i] == '*')
-        {
-            final_result = final_result + result[j];
-            j++;
-        }
-        else if (expression[i] == '-')
-        {
-            final_result = final_result - result[j];
-            j++;
-        }
-        else
-        {
-            previous_time++;
-        }
-    }
-
-    for (int i = 0; i < expression.size(); i++)
-    {
-        if (expression[i] == '+')
-        {
-            final_result = final_result + result[j];
-            j++;
-        }
-        else if (expression[i] == '-')
-        {
-            final_result = final_result - result[j];
-            j++;
-        }
-    }
-
-    std::cout<<final_result<<std::endl;*/
-    /*for (int i = 0; i < numbers.size(); i++)
-    {
-        std::cout<<numbers[i]<<std::endl;
-    }
-    for (int i = 0; i < ops.size(); i++)
-    {
-        std::cout<<ops[i]<<std::endl;
-    }*/
-
+    std::cout<<numbers.top()<<std::endl;
 
     return 0;
 }
